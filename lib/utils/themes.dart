a8ccd696c7537428e128573f99f2595e05d91278
RayyanAlFirdausi <rayyanalfirdausy@gmail.com>
2024-10-30 12:08:23 +0700

    Ubah Kode Warna

--- lib/utils/themes.dart
+++ lib/utils/themes.dart
@@ -9,10 +9,10 @@ Color neutral80 = const Color(0xFF0E0E0E);
 Color neutral70 = const Color(0xFF121212);
 Color neutral60 = const Color(0xFF171717);
 Color neutralBase = const Color(0xFF1B1B1B);
-Color neutral50 = const Color(0xFF414141);
-Color neutral40 = const Color(0xFF676767);
-Color neutral30 = const Color(0xFF8D8D8D);
-Color neutral20 = const Color(0xFFB3B3B3);
+Color neutral50 = const Color(0xFF575757);
+Color neutral40 = const Color(0xFF7F7F7F);
+Color neutral30 = const Color(0xFFBBBBBB);
+Color neutral20 = const Color(0xFFE8E8E8);
 Color neutral10 = const Color(0xFFF6F6F6);
 Color white = const Color(0xFFFFFFFF);
 
